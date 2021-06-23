7a478792f001a28d7051417eb3824156bec107fc
Shiv <sky.shivy@gmail.com>
2021-06-23 09:13:50 +0530

    framework added

--- MTGamificationF/ViewController.swift
+++ MTGamificationF/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
-
+import MTGamification
 class ViewController: UIViewController {
-
+    @IBOutlet weak var containerView: UIView!
+    var conView: UIView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Gamification
+       // Game.loadScratchCard(controller: self, mobileNumber: "51046", language: "EN", complition: complitionHandler(action:))
+        Game.gameList(controller: self, mobileNumber: "49028", language: "EN", complition: complitionHandler(action:))
     }
-
+    
+    
+    
+    @IBAction func refreshApi() {
+        //Game.loadScratchCard(controller: self, mobileNumber: "51046", language: "EN", complition: complitionHandler(action:))
+        Game.gameList(controller: self, mobileNumber: "49028", language: "EN", complition: complitionHandler(action:))
+    }
+    
+    
+    func complitionHandler(action: GameAction) {
+        switch action {
+        case .dismiss:
+            print(" complitionHandler controller dismissed")
+        case .onSuccess:
+            print("complitionHandler on success")
+        case .listView(let view):
+            self.conView?.removeFromSuperview()
+            self.conView = nil
+            self.conView = view
+            loadGameListView(self.conView!)
+        case .leaderBoardView(let view, let height):
+            loadLeaderBoard(view: view, height: height)
+        }
+    }
+    
+    fileprivate func loadGameListView(_ view: (UIView)) {
+        DispatchQueue.main.async {
+            view.frame = CGRect(x: 30, y: 40, width: UIScreen.main.bounds.width-60, height: (UIScreen.main.bounds.width-40)/2)
+            view.center = self.view.center
+             self.view.addSubview(view)
+        }
+    }
+    
+    func loadLeaderBoard(view: UIView, height: CGFloat) {
+        DispatchQueue.main.async {
+            view.frame = self.containerView.bounds
+            view.frame.size.height = height
+            self.containerView.addSubview(view)
+        }
+    }
 
 }
 
